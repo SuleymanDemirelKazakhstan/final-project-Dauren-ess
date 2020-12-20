4264db15d162dbf463ace458859cecc84c4e8264
System Administrator <root@MacBook-Air-Dauren.local>
2020-12-20 15:22:31 +0600

    Project

---
+++ main.js
@@ -0,0 +1,28 @@
+let image = document.getElementById("productImg");
+
+function button1(){
+    image.src = "https://object.pscloud.io/cms/cms/Photo/img_0_62_1496_4_1.jpg";
+  }
+  let a_button = document.querySelector("#i1");
+  a_button.addEventListener('click', button1);
+  
+  function button2(){
+    image.src = "https://object.pscloud.io/cms/cms/Photo/img_0_62_1496_3_1.jpg";
+  }
+  let b_button = document.querySelector("#i2");
+  b_button.addEventListener('click', button2);
+  
+  function button3(){
+    image.src = "https://object.pscloud.io/cms/cms/Photo/img_0_62_1496_5_1.jpg";
+  }
+    let c_buton = document.querySelector("#i3");
+  c_buton.addEventListener('click', button3);
+  
+  function button4(){
+    image.src = "https://object.pscloud.io/cms/cms/Photo/img_0_62_1496_1_1.jpg";
+  }
+    let d_button = document.querySelector("#i4");
+   d_button.addEventListener('click', button4);
+
+
+
